d39104677e8d1fa50b370b6e1e669a93029a2c1c
Princep27 <107925131+Princep27@users.noreply.github.com>
2025-12-21 17:43:55 +0530

    Time: 164 ms (5.06%), Space: 13.1 MB (19.1%) - LeetHub

---
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void fill(vector<int> &tmp,int n){
+        tmp.clear();
+        for(int i=1;i<n;++i) tmp.push_back(i);
+    }
+
+    int minDeletionSize(vector<string>& strs) {
+
+        int n = strs.size(), m = strs[0].size();
+        vector<int> rows,rem_rows;
+        int col = 0,cnt = 0;
+        fill(rows,n);
+
+        while(true){
+
+            if(col == m) return cnt;
+            for(int i=0;i<rows.size();++i){
+                int row = rows[i];
+                cout<<row<<" ";
+                if(strs[row][col] == strs[row-1][col] && col != m-1){
+                    rem_rows.push_back(row);
+                }else if(strs[row][col] < strs[row-1][col]){
+                    ++cnt; rem_rows = rows; 
+                    break;
+                }
+            }
+
+            cout<<endl;
+            for(auto it : rem_rows) cout<<it<<" "; cout<<endl<<endl;
+
+
+            if(rem_rows.size() == 0){
+                return cnt;
+            }else{
+                rows = rem_rows; 
+                rem_rows.clear();
+                ++col;
+            }
+        }
+
+    }
+};
