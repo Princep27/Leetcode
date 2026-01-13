a7e2119e2b63d5ceeccc7496e0f06df00c89e1bc
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-01-13 09:03:17 +0530

    Time: 167 ms (24.21%), Space: 149.2 MB (36.32%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    static double find(int[][] squares,double mid){
+        double sum = 0;
+        for(int i=0;i<squares.length;++i){
+            double l = squares[i][1], r = squares[i][1] + squares[i][2];
+            if(l < mid){
+                sum += (squares[i][2])*(Math.min(r,mid) - l);
+            }
+        }
+        return sum;
+    }
+
+    public double separateSquares(int[][] squares) {
+        double left = 0, right = 1000000001;
+        double total = 0,ans = 0;
+        int n = squares.length;
+
+        for(int i=0;i<n;++i) total += (long)squares[i][2]*(long)squares[i][2];
+        total /= 2;
+        
+        while(right - left >= 0.000001){
+            double mid = (left + right)/2;
+            double area = find(squares,mid);
+
+            if(area >= total){
+                right = mid;
+            }else{
+                left = mid;
+            }
+
+            ans = mid;
+        }
+
+        return ans;
+    }
+}
