a0fac51600cfd6e0cad64b4a5e5286c3a7d46b39
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-02-10 06:51:49 +0530

    Time: 493 ms (5.11%), Space: 46.8 MB (96.35%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,21 +1,22 @@
 class Solution {
     public int longestBalanced(int[] nums) {
-        int ans = 0;
+        int ans = 0,times = 0;
+        HashMap<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<nums.length;++i){
-            int odd = 0, even = 0;
-            HashMap<Integer,Integer> mp = new HashMap<>();
+            int odd = 0, even = 0; ++times;
             for(int j=i;j<nums.length;++j){
                 int val = nums[j];
-                int count = 0;
+                int time = 0;
 
-                if(mp.containsKey(val)){
-                    count = mp.get(val);
-                }else{
-                    if(val%2 == 1) ++odd;
+                if(mp.containsKey(val))
+                    time = mp.get(val);
+                
+                if(time != times){
+                    if(val % 2 == 1) ++odd;
                     else ++even;
                 }
 
-                mp.put(val,count+1);
+                mp.put(val,times);
                 if(even == odd) ans = Math.max(ans, j-i+1);
             }
         }
