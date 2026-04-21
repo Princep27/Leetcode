4044bcacb030633cd5d80e3580c6816229a989f4
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-04-21 08:37:57 +0530

    Time: 58 ms (41.94%), Space: 114 MB (69.89%) - LeetHub

--- 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
@@ -1,69 +1,48 @@
 class Solution {
     public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
-        int n = source.length;
-        int ans = n;
-        boolean vis[] = new boolean[n];
+        int n = source.length, ans = n;
+        boolean[] vis = new boolean[n];
 
-        for(int i=0;i<n;++i) vis[i] = false;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(n);
-        for(int i=0;i<n;++i) adj.add(new ArrayList<>());
-
-        for(int i=0;i<allowedSwaps.length;++i){
-            int u = allowedSwaps[i][0], v = allowedSwaps[i][1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+        for (int[] s : allowedSwaps) {
+            adj.get(s[0]).add(s[1]);
+            adj.get(s[1]).add(s[0]);
         }
 
-        for(int i=0;i<n;++i){
-            int cur = i;
+        for (int i = 0; i < n; i++) {
+            if (vis[i]) continue;
 
-            if(vis[cur] == false){
+            HashMap<Integer, Integer> mp = new HashMap<>();
+            ArrayList<Integer> comp = new ArrayList<>();
+            Deque<Integer> q = new ArrayDeque<>();
 
-                HashMap<Integer,Integer> mp = new HashMap<>();
-                ArrayList<Integer> indexes = new ArrayList<>();
-                Deque<Integer> q = new ArrayDeque<>();
-                q.addLast(cur);
-                vis[cur] = true;
+            q.add(i);
+            vis[i] = true;
 
-                while(q.size() != 0){
+            while (!q.isEmpty()) {
+                int v = q.poll();
+                comp.add(v);
 
-                    int sz = q.size();
-                    for(int j=0;j<sz;++j){
-                        int val = q.pollFirst();
-                        indexes.add(val);
-
-                        int m = adj.get(val).size();
-                        for(int k=0;k<m;++k){
-                            int vall = adj.get(val).get(k);
-                            if(!vis[vall]){
-                                q.addLast(vall);
-                                vis[vall] = true;
-                            }
-                        }
-                    }
-                }
-
-                for(int j=0;j<indexes.size();++j){
-                    int index = indexes.get(j);
-                    int val = target[index];
-                    int cnt = 0;
-                    if(mp.containsKey(val)) cnt = mp.get(val);
-                    mp.put(val,cnt+1);
-                }
-
-                for(int j=0;j<indexes.size();++j){
-                    int index = indexes.get(j);  
-                    int val = source[index];
-                    if(mp.containsKey(val)){
-                        --ans; 
-                        if(mp.get(val) == 1) mp.remove(val);
-                        else mp.put(val,mp.get(val)-1);
+                for (int nei : adj.get(v)) {
+                    if (!vis[nei]) {
+                        vis[nei] = true;
+                        q.add(nei);
                     }
                 }
             }
-        }
 
+            for (int idx : comp)
+                mp.put(target[idx], mp.getOrDefault(target[idx], 0) + 1);
+
+            for (int idx : comp) {
+                if (mp.getOrDefault(source[idx], 0) > 0) {
+                    ans--;
+                    mp.put(source[idx], mp.get(source[idx]) - 1);
+                }
+            }
+        }
         return ans;
     }
 }
