6e01f21d1176da3ee7ae7d5fd5545ef5255d1f96
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-04-28 08:30:06 +0530

    Time: 18 ms (71.07%), Space: 103.9 MB (30.17%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,18 +1,10 @@
 class Solution {
 
-    static boolean dfs(int[][] grid,boolean[][] vis,int x,int y,int n,int m){
+    static boolean dfs(int[][] grid,boolean[][] vis,int[][][] dir,int x,int y,int n,int m){
 
         if(x == n-1 && y == m-1) return true;
         vis[x][y] = true;
         boolean valid = false;
-
-        int[][][] dir = new int[7][][];
-        dir[1] = new int[][]{{0,-1},{0,1}};
-        dir[2] = new int[][]{{-1,0},{1,0}};
-        dir[3] = new int[][]{{0,-1},{1,0}};
-        dir[4] = new int[][]{{0,1},{1,0}};
-        dir[5] = new int[][]{{0,-1},{-1,0}};
-        dir[6] = new int[][]{{0,1},{-1,0}};
 
         for(int[] arr : dir[grid[x][y]]){
             int new_x = x + arr[0];
@@ -24,7 +16,7 @@ class Solution {
                 int back_x = new_x + tmp[0];
                 int back_y = new_y + tmp[1];
                 if(back_x == x && back_y == y){
-                    if(dfs(grid,vis,new_x,new_y,n,m)) 
+                    if(dfs(grid,vis,dir,new_x,new_y,n,m)) 
                         valid = true;
                 }
             }
@@ -37,9 +29,16 @@ class Solution {
 
         int n = grid.length, m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
+        int[][][] dir = new int[7][][];
+        dir[1] = new int[][]{{0,-1},{0,1}};
+        dir[2] = new int[][]{{-1,0},{1,0}};
+        dir[3] = new int[][]{{0,-1},{1,0}};
+        dir[4] = new int[][]{{0,1},{1,0}};
+        dir[5] = new int[][]{{0,-1},{-1,0}};
+        dir[6] = new int[][]{{0,1},{-1,0}};
 
         int x = 0, y = 0;
-        if(dfs(grid,vis,x,y,n,m)) return true;
+        if(dfs(grid,vis,dir,x,y,n,m)) return true;
         return false;
 
     }
