6f3665134dee6af51ff28d5fb093adfa4c0dcaf8
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-05-06 07:53:52 +0530

    Time: 9 ms (35.58%), Space: 124.8 MB (60.34%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -0,0 +1,48 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int n = boxGrid.length;
+        int m = boxGrid[0].length;
+
+        for(int row=0;row<n;++row){
+            int x = row, y = m-1;
+            for(int col=m-1;col>=0;--col){
+
+                //obstacle
+                if(boxGrid[row][col] == '*'){
+                    y = col;
+                }
+
+                //stone
+                else if(boxGrid[row][col] == '#'){
+                    if(boxGrid[x][y] == '.'){
+                        boxGrid[row][col] = '.';
+                        boxGrid[x][y] = '#';
+                    }
+                    --y;
+                }
+
+                //empty
+                else{
+                    if(boxGrid[x][y] != '.') y = col;
+                }
+            }
+        }
+
+        char[][] ans = new char[m][n];
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                ans[i][j] = boxGrid[j][i];
+            }
+        }
+
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n/2;++j){
+                char tmp = ans[i][j];
+                ans[i][j] = ans[i][n-1-j];
+                ans[i][n-1-j] = tmp;
+            }
+        }
+
+        return ans;
+    }
+}
