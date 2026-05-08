cd1f37d651da66920c9e9c3234f5afb7f7036a76
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-05-08 09:28:39 +0530

    Time: 1655 ms (5.29%), Space: 183.1 MB (78.85%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int specialTriplets(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+        int mod = 1000000007;
+
+        TreeMap<Integer,Integer> left = new TreeMap<>();
+        TreeMap<Integer,Integer> right = new TreeMap<>();
+
+        for(int i=2;i<n;++i) right.put(nums[i], right.getOrDefault(nums[i],0) + 1);
+
+        left.put(nums[0],1);
+
+        for(int i=1;i<n-1;++i){
+            int l = left.getOrDefault(nums[i]*2,0);
+            int r = right.getOrDefault(nums[i]*2,0);
+            ans = (ans + ((long)l * (long)r))%mod;
+            
+            left.put(nums[i],left.getOrDefault(nums[i],0) + 1);
+            right.put(nums[i+1], right.get(nums[i+1]) - 1);
+        }
+
+        return (int)ans;
+    }
+}
