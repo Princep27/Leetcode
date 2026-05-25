d837c789f99064fc9c6ccef275bb995d242da060
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-05-25 09:27:07 +0530

    Time: 310 ms (5.68%), Space: 93.5 MB (5.33%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -0,0 +1,37 @@
+class Solution {
+    void dfs(int curIndex,int minJump,int maxJump,boolean[] vis,TreeSet<Integer> st,int n){
+        vis[curIndex] = true;
+
+        Integer nextIndex = curIndex+minJump;
+        while(true){
+
+            nextIndex = st.ceiling(nextIndex);
+            if(nextIndex == null) break;
+
+            if((nextIndex - curIndex) <= maxJump){
+                st.remove(nextIndex);
+                dfs(nextIndex,minJump,maxJump,vis,st,n);
+            }else{
+                break;
+            }
+            
+        }
+
+    }
+
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        List<Integer> arr = new ArrayList<>();
+        if(s.charAt(n-1) == '1') return false;
+        TreeSet<Integer> st = new TreeSet<>();
+
+        for(int i=1;i<n;++i)  
+            if(s.charAt(i) == '0') st.add(i);
+
+        boolean[] vis = new boolean[n];
+        for(int i=0;i<n;++i) vis[i] = false;
+
+        dfs(0,minJump,maxJump,vis,st,n);
+        return vis[n-1];
+    }
+}
