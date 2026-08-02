ae78332b7ebe0f65ad26310243d6c2f0a08f5838
Princep27 <107925131+Princep27@users.noreply.github.com>
2026-08-02 07:49:34 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.35%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = nums[i];
+
+        for (int len = 2; len <= n; len++) {
+            for (int l = 0; l + len - 1 < n; l++) {
+                int r = l + len - 1;
+                dp[l][r] = Math.max(
+                    nums[l] - dp[l + 1][r],
+                    nums[r] - dp[l][r - 1]
+                );
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+}
